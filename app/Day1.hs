49b3e261ae96567a9dfb8287ae782c8c2cab97fb
heitor <heitor@bluebaron.local>
2022-12-02 12:39:22 +0000

    Day 1

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,3 +1,21 @@
 module Day1 where
+
+import Control.Monad
+import Data.List
+
+splitWhen :: (a -> Bool) -> [a] -> [[a]]
+splitWhen cond [] = []
+splitWhen cond as = case t of
+    []     -> [h]
+    (y:ys) -> h:splitWhen cond ys
+    where (h, t) = break cond as
+
+(<$$>) :: (a -> b) -> [[a]] -> [[b]]
+f <$$> aas = (f<$>) <$> aas
+
 main :: IO ()
-main = print "Not yet."
+main = do
+    lines <- lines <$!> readFile "data/Day1.txt"
+    let elfCalories = sum <$> read <$$> splitWhen (=="") lines
+    print $ maximum elfCalories
+    print $ sum $ take 3 $ sortOn negate elfCalories
