2056a0af8a742dd4657bddde4b8bf64de94d5fae
heitor <heitor@bluebaron.local>
2022-12-06 21:59:05 +0000

    Day 6

--- app/Day6.hs
+++ app/Day6.hs
@@ -1,3 +1,28 @@
 module Day6 where
+
+import Control.Monad
+import Data.List
+import Data.Functor
+import Data.Function
+
+nwise :: Int -> [a] -> [[a]]
+nwise n xs = take n <$> iterate tail xs
+
+countUnique :: Eq a => [a] -> Int
+countUnique = length . nub
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [0..]
+
+firstWhich :: (a -> Bool) -> [a] -> a
+firstWhich pred xs = head $ filter pred xs
+
+startOfMessage :: Int -> String -> Int
+startOfMessage n s = nwise n s <&> countUnique & enumerate & firstWhich isValid & uncurry (+)
+    where isValid (_, nuniq) = nuniq == n
+
 main :: IO ()
-main = print "Not yet."
+main = do
+    string <- readFile "data/Day6.txt"
+    print $ startOfMessage 4 string
+    print $ startOfMessage 14 string
